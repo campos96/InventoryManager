a220be79c977709e90fa43165c3cfff84459c506
cesarhc96 <cesarhcmartinez@gmail.com>
2022-05-22 01:45:18 -0700

    Add ASP.NET Core 3.1 version

---
+++ InventoryManager.Core3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryManager.Core3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
